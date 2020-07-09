161359e5d003f045f03655a0e053a1b1c0f14bde
Sai Vedagiri <43285226+SaiVedagiri@users.noreply.github.com>
2020-07-08 21:45:15 -0400

    Push Firebase, Location, QR, and TTS

--- grove_app/lib/main.dart
+++ grove_app/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:async';
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:intl/intl.dart';
 import 'package:location/location.dart';
+import 'dart:math';
+import 'package:flutter_tts/flutter_tts.dart';
 
 var _firebaseRef = FirebaseDatabase().reference();
+FlutterTts flutterTts = FlutterTts();
+var result = "";
+
+var firebaseData;
 
 void main() {
   runApp(MyApp());
@@ -28,7 +33,8 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         routes: {
-          "/": (_) => StreamBuilder(
+          "/": (_) =>
+              StreamBuilder(
                 stream: getLinksStream(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
@@ -54,8 +60,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<String> createAlertDialog(
-      BuildContext context, String title, String body) {
+  final fb = FirebaseDatabase.instance;
+
+  initState() {
+    super.initState();
+    initStateFunction();
+  }
+
+  initStateFunction() async {
+    final ref = fb.reference();
+    ref.child("locations").once().then((DataSnapshot data) {
+      firebaseData = data.value;
+    });
+  }
+
+  Future<String> createAlertDialog(BuildContext context, String title,
+      String body) {
     return showDialog(
         context: context,
         builder: (context) {
@@ -174,7 +194,6 @@ class _HomePageState extends State<HomePage> {
 class VideoPage extends StatefulWidget {
   VideoPage({Key key}) : super(key: key);
 
-
   @override
   _VideoPageState createState() => _VideoPageState();
 }
@@ -228,8 +247,8 @@ class _VideoPageState extends State<VideoPage> {
     super.dispose();
   }
 
-  Future<String> createAlertDialog(
-      BuildContext context, String title, String body) {
+  Future<String> createAlertDialog(BuildContext context, String title,
+      String body) {
     return showDialog(
         context: context,
         builder: (context) {
@@ -318,9 +337,7 @@ class _VideoPageState extends State<VideoPage> {
             ),
             IconButton(
               icon: Icon(Icons.videocam),
-              onPressed: () {
-
-              },
+              onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.map),
@@ -373,6 +390,14 @@ class _MapPageState extends State<MapPage> {
   PermissionStatus permissionGranted;
   LocationData locationData;
   GoogleMapController mapController;
+  double latitude = 45.0;
+  double longitude = -122.0;
+  final Map<String, Marker> _markers = {};
+
+  @override
+  initState() {
+    super.initState();
+  }
 
   void _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
@@ -392,28 +417,194 @@ class _MapPageState extends State<MapPage> {
       }
     }
 
-    LocationData tempLocationData = await location.getLocation();
-    setState(() {
-      locationData = tempLocationData;
+    locationData = await location.getLocation();
+
+    double deg2rad(deg) {
+      return deg * (pi / 180);
+    }
+
+    double getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2 - lat1); // deg2rad below
+      var dLon = deg2rad(lon2 - lon1);
+      var a = sin(dLat / 2) * sin(dLat / 2) +
+          cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon / 2) *
+              sin(dLon / 2);
+      var c = 2 * atan2(sqrt(a), sqrt(1 - a));
+      var d = R * c; // Distance in km
+      return d;
+    }
+
+    print(locationData.latitude);
+    print(locationData.longitude);
+
+    firebaseData.keys.forEach((var key) {
+      print(firebaseData[key]["latitude"]);
+      print(firebaseData[key]["longitude"]);
+      print(getDistanceFromLatLonInKm(locationData.latitude, locationData.longitude, firebaseData[key]["latitude"], firebaseData[key]["longitude"]));
+      if (getDistanceFromLatLonInKm(
+          locationData.latitude, locationData.longitude,
+          firebaseData[key]["latitude"], firebaseData[key]["longitude"]) < 1000) {
+        var body = "You are about ${1000 * getDistanceFromLatLonInKm(locationData.latitude, locationData.longitude, firebaseData[key]["latitude"], firebaseData[key]["longitude"])}m to ${firebaseData[key]["name"]}. Would you like to more info?";
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                  title: Text("Location Near"),
+                  content: Text(body),
+                  actions: <Widget>[
+                    MaterialButton(
+                      elevation: 5.0,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                                builder: (context) =>
+                                new DetailsPage(
+                                    title: firebaseData[key]["name"], info: firebaseData[key]["text"])));
+                      },
+                      child: Text("OK"),
+                    ),
+                    MaterialButton(
+                      elevation: 5.0,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Cancel"),
+                    )
+                  ]);
+            });
+      }
     });
-    print("Latitude: ${locationData.latitude}");
-    print("Longitude: ${locationData.longitude}");
+
+    setState(() {
+      latitude = locationData.latitude;
+      longitude = locationData.longitude;
+    });
     location.onLocationChanged.listen((LocationData currentLocation) async {
       LocationData tempLocationData = await location.getLocation();
-      print("Latitude: ${locationData.latitude}");
-      print("Longitude: ${locationData.longitude}");
+
+      double deg2rad(deg) {
+        return deg * (pi / 180);
+      }
+
+      double getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2 - lat1); // deg2rad below
+        var dLon = deg2rad(lon2 - lon1);
+        var a = sin(dLat / 2) * sin(dLat / 2) +
+            cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon / 2) *
+                sin(dLon / 2);
+        var c = 2 * atan2(sqrt(a), sqrt(1 - a));
+        var d = R * c; // Distance in km
+        return d;
+      }
+
+      firebaseData.keys.forEach((var key) {
+        if (getDistanceFromLatLonInKm(
+            tempLocationData.latitude, tempLocationData.longitude,
+            firebaseData[key]["latitude"], firebaseData[key]["longitude"]) <
+            10) {
+          var body = "You are about ${1000 * getDistanceFromLatLonInKm(tempLocationData.latitude, tempLocationData.longitude, firebaseData[key]["latitude"], firebaseData[key]["longitude"])}m to ${firebaseData[key]["name"]}. Would you like to view details.";
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                    title: Text("Location Near"),
+                    content: Text(body),
+                    actions: <Widget>[
+                      MaterialButton(
+                        elevation: 5.0,
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          Navigator.push(
+                              context,
+                              new MaterialPageRoute(
+                                  builder: (context) =>
+                                  new DetailsPage(
+                                      title: firebaseData[key]["name"], info: firebaseData[key]["text"])));
+                        },
+                        child: Text("OK"),
+                      ),
+                      MaterialButton(
+                        elevation: 5.0,
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("Cancel"),
+                      )
+                    ]);
+              });
+        }
+      });
+
+      setState(() {
+        _markers.clear();
+        var marker = Marker(
+          markerId: MarkerId("currentLocation"),
+          position: LatLng(latitude, longitude),
+          infoWindow: InfoWindow(
+            title: "Current Location",
+          ),
+        );
+        _markers["currentLocation"] = marker;
+
+//      for (var office in locations.offices) {
+//        var newMarker = Marker(
+//          markerId: MarkerId(office.name),
+//          position: LatLng(office.lat, office.lng),
+//          infoWindow: InfoWindow(
+//            title: office.name,
+//            snippet: office.address,
+//          ),
+//        );
+//        _markers[office.name] = newMarker;
+//      }
+      });
     });
+
+    setState(() {
+      _markers.clear();
+      var marker = Marker(
+        markerId: MarkerId("currentLocation"),
+        position: LatLng(latitude, longitude),
+        infoWindow: InfoWindow(
+          title: "Current Location",
+        ),
+      );
+      _markers["currentLocation"] = marker;
+
+//      for (var office in locations.offices) {
+//        var newMarker = Marker(
+//          markerId: MarkerId(office.name),
+//          position: LatLng(office.lat, office.lng),
+//          infoWindow: InfoWindow(
+//            title: office.name,
+//            snippet: office.address,
+//          ),
+//        );
+//        _markers[office.name] = newMarker;
+//      }
+    });
+
     location.onLocationChanged.timeout(Duration(seconds: 5));
   }
 
   @override
-  void dispose(){
-
+  void dispose() {
     super.dispose();
+    location = null;
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
   }
 
-  Future<String> createAlertDialog(
-      BuildContext context, String title, String body) {
+  Future<String> createAlertDialog(BuildContext context, String title,
+      String body) {
     return showDialog(
         context: context,
         builder: (context) {
@@ -462,6 +653,59 @@ class _MapPageState extends State<MapPage> {
       appBar: AppBar(
         title: Text("Grove Map"),
         actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.camera_alt),
+              onPressed: () async {
+                try {
+                  String qrResult = await BarcodeScanner.scan();
+                  result = qrResult;
+                  var done = false;
+                  firebaseData.keys.forEach((var key) {
+                    if (firebaseData[key]["qr-id"] == qrResult) {
+                      done = true;
+                      var locationName = firebaseData[key]["name"];
+                      var locationDetails = firebaseData[key]["text"];
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) =>
+                              new DetailsPage(
+                                  title: locationName, info: locationDetails)));
+                    }
+                  });
+                  if (done == false) {
+                    createAlertDialog(context, "Invalid QR Code",
+                        "This QR code is not recognized by the Grove app. Please try again.");
+                  }
+                }
+                on PlatformException catch (ex) {
+                  if (ex.code == BarcodeScanner.CameraAccessDenied) {
+                    setState(() {
+                      createAlertDialog(context, "Scan QR",
+                          "Please enable camera permissions for Grove App.");
+                    });
+                  } else {
+                    setState(() {
+                      result = "Unknown Error $ex";
+                      createAlertDialog(
+                          context, "Scan QR", "Unkown Error Occured: $ex");
+                    });
+                  }
+                } on FormatException {
+                  setState(() {
+                    result =
+                    "You pressed the back button before scanning anything";
+                    createAlertDialog(
+                        context, "Scan QR", "No QR Code was recognized.");
+                  });
+                } catch (ex) {
+                  setState(() {
+                    result = "Unknown Error $ex";
+                    createAlertDialog(
+                        context, "Scan QR", "Unkown Error Occured: $ex");
+                  });
+                }
+              }),
           IconButton(
             icon: Icon(Icons.help),
             onPressed: () {
@@ -519,13 +763,28 @@ class _MapPageState extends State<MapPage> {
           ],
         ),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        mapType: MapType.hybrid,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(locationData != null ? locationData.latitude : 45.521563, locationData != null ? locationData.longitude: -122.677433),
-          zoom: 11.0,
-        ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height - 128,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              mapType: MapType.hybrid,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(40.4367445053398, -74.23047140991979),
+                zoom: 11.0,
+              ),
+              markers: _markers.values.toSet(),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -539,8 +798,8 @@ class TreasurePage extends StatefulWidget {
 }
 
 class _TreasurePageState extends State<TreasurePage> {
-  Future<String> createAlertDialog(
-      BuildContext context, String title, String body) {
+  Future<String> createAlertDialog(BuildContext context, String title,
+      String body) {
     return showDialog(
         context: context,
         builder: (context) {
@@ -650,6 +909,139 @@ class _TreasurePageState extends State<TreasurePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsPage extends StatefulWidget {
+  DetailsPage({Key key, this.title, this.info}) : super(key: key);
+
+  final String title;
+  final String info;
+
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+
+  initState() {
+    super.initState();
+    initStateFunction();
+  }
+
+  initStateFunction() async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setSpeechRate(1.0);
+    await flutterTts.setVolume(1.0);
+    await flutterTts.setPitch(1.0);
+    await flutterTts.isLanguageAvailable("en-US");
+  }
+
+  String status = "Play";
+
+  Future<String> helpContext(BuildContext context, String title, Widget body) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              title: Text(title),
+              content: body,
+              actions: <Widget>[
+                MaterialButton(
+                  elevation: 5.0,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"),
+                )
+              ]);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.help),
+            onPressed: () {
+              helpContext(
+                  context,
+                  "Help",
+                  Text.rich(
+                    TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Home Page\n',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline),
+                        ),
+                        TextSpan(
+                          text: 'Text goes here.\n',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ));
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxHeight: MediaQuery
+                    .of(context)
+                    .size
+                    .height - 350),
+                child: SingleChildScrollView(
+                  child: Container(
+                    child: Text(
+                      '${widget.info}',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child:
+              FloatingActionButton.extended(
+                  icon: Icon(status == "Play" ? Icons.play_arrow : Icons.pause),
+                  label: Text(status),
+                  onPressed: () async {
+                    if (status == "Play") {
+                      await flutterTts.speak(widget.info);
+                      setState(() {
+                        status = "Pause";
+                      });
+                    } else {
+                      await flutterTts.stop();
+                      setState(() {
+                        status = "Play";
+                      });
+                    }
+                  }),),
+          ],
         ),
       ),
     );
