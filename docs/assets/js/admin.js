d5a418eb36594cc756c332580291c395f1dbaa13
Arya Tschand <43081871+aryatschand@users.noreply.github.com>
2020-09-14 20:11:33 -0400

    add link for location in website

--- docs/assets/js/admin.js
+++ docs/assets/js/admin.js
@@ -4,7 +4,6 @@ let editScavenger = true;
 let editRow = false;
 let missing = [];
 
-
 if (sessionStorage.getItem("valid") != "true") {
     window.location.href = "login.html"
 } else {
@@ -19,6 +18,16 @@ if (sessionStorage.getItem("valid") != "true") {
         temp.push(newPost.text);
         temp.push(newPost.social);
         temp.push(snapshot.key);
+
+        firebase.database().ref("locations").startAt("link").endAt("link").once('value', function(snap){
+            let returnVar = "none"
+            if(snap.exists()){
+                returnVar = snap.link;
+            return
+            }
+            temp.push(returnVar);
+        });
+
         locations.push(temp);
         LocationsPopulate();
         scavengerPopulate();
@@ -44,6 +53,7 @@ function newLocationAdd() {
         let details = document.getElementById("addDetails").value;
         let social = document.getElementById("addSocial").value;
         let qr = Math.round(Math.random() * 10000000000).toString();
+        let locationLink = document.getElementById("addLink").value;
 
         if (latitude != NaN && latitude > -90 && latitude < 90 && longitude != NaN && longitude > -180 && longitude < 180 && name != "") {
             firebase.database().ref("locations").push({
@@ -52,7 +62,8 @@ function newLocationAdd() {
                 name: name,
                 "qr-id": qr,
                 social: social,
-                text: details
+                text: details,
+                link: locationLink
             });
         } else {
             alert("There is invalid or empty information. Please enter valid data to add location.");
@@ -64,13 +75,13 @@ function LocationsPopulate() {
     $("#locationTable tr").remove();
 
     var tablestring = "";
-    tablestring += '<tr class="text-center"> <th style="width: 166px;">Location Name</th><th style="width: 131px;">QR Image</th><th style="width: 110.4px;">Latitude</th><th style="width: 116.8;">Longitude</th><th>Description</th><th style="width: 204px;">Social Text</th><th style="width: 108px;">Actions</th></tr>';
+    tablestring += '<tr class="text-center"> <th style="width: 166px;">Location Name</th><th style="width: 131px;">QR Image</th><th style="width: 110.4px;">Latitude</th><th style="width: 116.8;">Longitude</th><th>Description</th><th style="width: 204px;">Social Text</th><th style="width: 150px;">Link</th><th style="width: 108px;">Actions</th></tr>';
 
     for (var x = 0; x < locations.length; x++) {
-        tablestring += `<tr id="row${x}" class="text-center"> <td>${locations[x][0]}</td><td><img src="https://api.qrserver.com/v1/create-qr-code/?data=${locations[x][1]}&amp;size=100x100" alt="" title="" /></td><td>${locations[x][2]}<br></td><td>${locations[x][3]}<br></td><td>${locations[x][4]}</td><td>${locations[x][5]}</td><td class="text-center"><button onclick="LocationEditPopulate(${x})" class="btn btn-primary" type="button" style="margin-bottom: 15px;">Edit</button><button onclick="LocationDeletePopulate(${x})" class="btn btn-primary" type="button">Delete</button></td></tr>`
+        tablestring += `<tr id="row${x}" class="text-center"> <td>${locations[x][0]}</td><td><img src="https://api.qrserver.com/v1/create-qr-code/?data=${locations[x][1]}&amp;size=100x100" alt="" title="" /></td><td>${locations[x][2]}<br></td><td>${locations[x][3]}<br></td><td>${locations[x][4]}</td><td>${locations[x][5]}</td><td>${locations[x][7]}</td><td class="text-center"><button onclick="LocationEditPopulate(${x})" class="btn btn-primary" type="button" style="margin-bottom: 15px;">Edit</button><button onclick="LocationDeletePopulate(${x})" class="btn btn-primary" type="button">Delete</button></td></tr>`
     }
 
-    tablestring += '<tr><td><input id="addName" type="text" style="width: 159px;"></td><td class="text-center">Auto</td><td><input id="addLatitude" type="text" style="width: 150px;"></td><td><input id="addLongitude" type="text" style="width: 150px;"></td><td class="text-center"><textarea id="addDetails" style="width:402px;"></textarea></td><td><input id="addSocial" type="text"></td><td class="text-center"><button id="addBtn" onclick="newLocationAdd()" class="btn btn-primary" type="button" style="margin-bottom: 15px;">Add</button></td></tr>'
+    tablestring += '<tr><td><input id="addName" type="text" style="width: 159px;"></td><td class="text-center">Auto</td><td><input id="addLatitude" type="text" style="width: 150px;"></td><td><input id="addLongitude" type="text" style="width: 150px;"></td><td class="text-center"><textarea id="addDetails" style="width:402px;"></textarea></td><td><input id="addSocial" type="text"></td><td><input id="addLink" type="text"></td><td class="text-center"><button id="addBtn" onclick="newLocationAdd()" class="btn btn-primary" type="button" style="margin-bottom: 15px;">Add</button></td></tr>'
 
     $("#locationTable tbody").append(
         tablestring
@@ -80,7 +91,7 @@ function LocationsPopulate() {
 function LocationEditPopulate(tableRow) {
     if (editScavenger) {
         editRow = true;
-        let rowstring = `<tr><td><input id="editName${tableRow}" type="text" style="width: 159px;" value="${locations[tableRow][0]}"></td><td class="text-center" id="editQR${locations[tableRow][1]}">${locations[tableRow][1]}</td><td><input id="editLatitude${tableRow}" type="text" style="width: 150px;" value="${locations[tableRow][2]}"></td><td><input id="editLongitude${tableRow}" type="text" style="width: 150px;" value="${locations[tableRow][3]}"></td><td class="text-center"><textarea id="editDetails${tableRow}" style="width:402px;">${locations[tableRow][4]}</textarea></td><td><input id="editSocial${tableRow}" type="text" value="${locations[tableRow][5]}"></td><td class="text-center"><button onclick="SaveEdit(${tableRow})" class="btn btn-primary" type="button" style="margin-bottom: 15px;">Save</button><button onclick="DeleteEdit(${tableRow})" class="btn btn-primary" type="button">Discard</button></td>`
+        let rowstring = `<tr><td><input id="editName${tableRow}" type="text" style="width: 159px;" value="${locations[tableRow][0]}"></td><td class="text-center" id="editQR${locations[tableRow][1]}">${locations[tableRow][1]}</td><td><input id="editLatitude${tableRow}" type="text" style="width: 150px;" value="${locations[tableRow][2]}"></td><td><input id="editLongitude${tableRow}" type="text" style="width: 150px;" value="${locations[tableRow][3]}"></td><td class="text-center"><textarea id="editDetails${tableRow}" style="width:402px;">${locations[tableRow][4]}</textarea></td><td><input id="editSocial${tableRow}" type="text" value="${locations[tableRow][5]}"></td><td><input id="editSocial${tableRow}" type="text" value="${locations[tableRow][7]}"></td><td class="text-center"><button onclick="SaveEdit(${tableRow})" class="btn btn-primary" type="button" style="margin-bottom: 15px;">Save</button><button onclick="DeleteEdit(${tableRow})" class="btn btn-primary" type="button">Discard</button></td>`
         document.getElementById(`row${tableRow}`).innerHTML = rowstring;
     }
 }
@@ -93,19 +104,32 @@ function SaveEdit(row) {
     let social = document.getElementById(`editSocial${row}`).value;
     let qr = locations[row][1];
     let key = locations[row][6];
+    let locationLink = locations[row][7]
 
     if (latitude != NaN && latitude > -90 && latitude < 90 && longitude != NaN && longitude > -180 && longitude < 180 && name != "") {
         let temp = []
         temp.push(name, qr, latitude, longitude, details, social, key);
         locations[row] = temp;
 
-        firebase.database().ref("locations").child(key).update({
-            name: name,
-            latitude: latitude,
-            longitude: longitude,
-            text: details,
-            social: social
-        });
+        if (locationLink == "") {
+            firebase.database().ref("locations").child(key).update({
+                name: name,
+                latitude: latitude,
+                longitude: longitude,
+                text: details,
+                social: social,
+            });
+        } else {
+            firebase.database().ref("locations").child(key).update({
+                name: name,
+                latitude: latitude,
+                longitude: longitude,
+                text: details,
+                social: social,
+                link: locationLink
+            });
+        }
+        
         editRow = false;
 
         LocationsPopulate();
